6386f4df7ae68475b31e266f11cfe351fe927b1a
Arjun Vijay Prakash <137415649+ArjunCodess@users.noreply.github.com>
2024-03-11 15:47:04 +0000

    final commit

---
+++ pages/api/predictions/index.js
@@ -0,0 +1,30 @@
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+
+export default async function handler(req, res) {
+  if (!process.env.REPLICATE_API_TOKEN) {
+    throw new Error(
+      "The REPLICATE_API_TOKEN environment variable is not set. See README.md for instructions on how to set it."
+    );
+  }
+
+  const prediction = await replicate.predictions.create({
+    version: "92fa143ccefeed01534d5d6648bd47796ef06847a6bc55c0e5c5b6975f2dcdfb",
+
+    // This is the text prompt that will be submitted by a form on the frontend
+    input: { prompt: req.body.prompt },
+  });
+
+  if (prediction?.error) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: prediction.error }));
+    return;
+  }
+
+  res.statusCode = 201;
+  res.end(JSON.stringify(prediction));
+  console.log(prediction);
+}
